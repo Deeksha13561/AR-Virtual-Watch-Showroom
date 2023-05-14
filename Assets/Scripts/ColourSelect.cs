993a2d49990d0f7b0784a0c48d5b7da01c5eb140
Deeksha13561 <65804768+Deeksha13561@users.noreply.github.com>
2023-05-14 13:32:15 +0530

    Fresh commit

---
+++ Assets/Scripts/ColourSelect.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColourSelect : MonoBehaviour
+{
+
+	public Material band1;
+	public Material band2;
+	public Material band3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void w1ColourSelect(){
+
+	string buttonName=EventSystem.current.currentSelectedGameObject.name;
+	if(buttonName == "Black button"){
+  		//set colour
+  		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#000000", out myColour);
+  		band1.color= myColour;
+  	}
+	else if(buttonName == "Blue button"){
+		//set colour
+		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#103B90", out myColour);
+  		band1.color= myColour;
+  	}
+
+    }
+
+public void w2ColourSelect(){
+string buttonName=EventSystem.current.currentSelectedGameObject.name;
+	if(buttonName == "Black button"){
+  		//set colour
+  		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#000000", out myColour);
+  		band2.color= myColour;
+  	}
+	else if(buttonName == "Red button"){
+		//set colour
+		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#740202", out myColour);
+  		band2.color= myColour;
+  	}
+    } 
+
+    public void w3ColourSelect(){
+string buttonName=EventSystem.current.currentSelectedGameObject.name;
+	if(buttonName == "Black button"){
+  		//set colour
+  		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#000000", out myColour);
+  		band3.color= myColour;
+  	}
+	else if(buttonName == "Silver button"){
+		//set colour
+		Color myColour= new Color();
+  		ColorUtility.TryParseHtmlString("#A1A1A1", out myColour);
+  		band3.color= myColour;
+  	}
+    }
+}
---
+++ Assets/watchselect.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class watchselect : MonoBehaviour
+{
+public GameObject watchModel1;
+public GameObject watchModel2;
+public GameObject watchModel3;
+
+public GameObject w1Window;
+public GameObject w2Window;
+public GameObject w3Window;
+
+
+    void Start()
+    {
+       
+
+    }
+public void WatchOneButtonClicked(){
+
+	//show watch model 1 on users wrist
+
+	watchModel1.SetActive(true);
+	watchModel2.SetActive(false);
+	watchModel3.SetActive(false);
+//animate w1 window
+	w1Window.SetActive(true);
+	w2Window.SetActive(false);
+	w3Window.SetActive(false);
+}
+
+public void WatchTwoButtonClicked(){
+	//show watch model 2 on users wrist
+
+	watchModel1.SetActive(false);
+	watchModel2.SetActive(true);
+	watchModel3.SetActive(false);
+
+	//animate w2 window
+	w1Window.SetActive(false);
+	w2Window.SetActive(true);
+	w3Window.SetActive(false);
+	
+}
+
+public void WatchThreeButtonClicked(){
+	//show watch model 3 on users wrist
+	watchModel1.SetActive(false);
+	watchModel2.SetActive(false);
+	watchModel3.SetActive(true);
+	
+
+	//animate w3 window
+w1Window.SetActive(false);
+	w2Window.SetActive(false);
+	w3Window.SetActive(true);
+}
+  
+  public void CloseButtonClicked(){
+  	string buttonName=EventSystem.current.currentSelectedGameObject.name;
+  	if(buttonName == "w1close"){
+  		//
+  		w1Window.SetActive(false);
+  	}
+  	else if(buttonName == "w2close"){
+		w2Window.SetActive(false);
+  	}
+  	else if(buttonName == "w3close"){
+  		w3Window.SetActive(false);
+  	}
+  }
+}
